125f7d69d0bf2be321113692c0d24c38f7781284
dinaiym <dinaiym.dubanaeva@alatoo.edu.kg>
2025-05-15 13:22:26 +0600

    try 6

--- asset_management/urls.py
+++ asset_management/urls.py
@@ -1,49 +1,38 @@
-# # asset_management/urls.py
-# from django.contrib import admin
-# from django.urls import path, include
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-# from django.conf import settings
-# from django.conf.urls.static import static
-
-# # импортим DetailView и модель
-# from django.views.generic import DetailView
-# from assets.models import Asset
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/', include('assets.urls')),
-#     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-#     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-#     # DetailView для веб-страницы карточки
-#     path(
-#         'asset/<uuid:pk>/',
-#         DetailView.as_view(
-#             model=Asset,
-#             template_name='assets/asset_detail.html',
-#             context_object_name='asset'
-#         ),
-#         name='asset-detail-web'
-#     ),
-# ]
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# asset_management/urls.py
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
+from django.http import HttpResponse
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView, TokenRefreshView
-)
+
+# простой ответ на GET /
+def home(request):
+    return HttpResponse("Сервис Asset Management запущен!")
 
 urlpatterns = [
+    # корень
+    path('', home, name='home'),
+    # либо сразу редирект на админку/API:
+    # path('', RedirectView.as_view(url='/admin/', permanent=False)),
+    
     path('admin/', admin.site.urls),
     path('api/', include('assets.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # веб-карточка
+    path(
+        'asset/<uuid:pk>/',
+        DetailView.as_view(
+            model=Asset,
+            template_name='assets/asset_detail.html',
+            context_object_name='asset'
+        ),
+        name='asset-detail-web'
+    ),
 ]
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+# отдача media-файлов и статических (чтобы не было 404 на /media/…)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
