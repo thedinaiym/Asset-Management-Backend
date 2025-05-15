b362786a08bbf8590dce231abcafad31e4b5cf58
dinaiym <dinaiym.dubanaeva@alatoo.edu.kg>
2025-05-15 13:25:07 +0600

    try 7

--- asset_management/urls.py
+++ asset_management/urls.py
@@ -1,27 +1,38 @@
 # asset_management/urls.py
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import DetailView, RedirectView
 from django.http import HttpResponse
-from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
-# простой ответ на GET /
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from assets.models import Asset
+from assets import views  # чтобы DRF-роуты подхватились
+
+# Простой приветственный вьюхер на /
 def home(request):
     return HttpResponse("Сервис Asset Management запущен!")
 
 urlpatterns = [
-    # корень
+    # Главная страница
     path('', home, name='home'),
-    # либо сразу редирект на админку/API:
+    # или вместо home можно сразу редиректить:
     # path('', RedirectView.as_view(url='/admin/', permanent=False)),
-    
+
+    # Админка
     path('admin/', admin.site.urls),
-    path('api/', include('assets.urls')),
+
+    # JWT-токены
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    # веб-карточка
+
+    # Основные DRF-роуты из assets/urls.py
+    path('api/', include('assets.urls')),
+
+    # Веб-карточка (HTML) для DetailView
     path(
         'asset/<uuid:pk>/',
         DetailView.as_view(
@@ -33,6 +44,6 @@ urlpatterns = [
     ),
 ]
 
-# отдача media-файлов и статических (чтобы не было 404 на /media/…)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Отдаём статику и медиа всегда (не только в DEBUG)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
