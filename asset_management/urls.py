0262fa1f537c3bfaa7470c9c02af5f23ccfe0b1b
dinaiym <dinaiym.dubanaeva@alatoo.edu.kg>
2025-05-15 13:30:51 +0600

    try 7

--- asset_management/urls.py
+++ asset_management/urls.py
@@ -1,38 +1,35 @@
 # asset_management/urls.py
 
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import DetailView, RedirectView
+from django.urls import path, include
 from django.http import HttpResponse
+from django.views.generic import DetailView
 from django.conf import settings
 from django.conf.urls.static import static
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from assets.models import Asset
-from assets import views  # чтобы DRF-роуты подхватились
 
-# Простой приветственный вьюхер на /
+# Простая заглушка на корень
 def home(request):
     return HttpResponse("Сервис Asset Management запущен!")
 
 urlpatterns = [
-    # Главная страница
+    # корень
     path('', home, name='home'),
-    # или вместо home можно сразу редиректить:
-    # path('', RedirectView.as_view(url='/admin/', permanent=False)),
 
-    # Админка
+    # админка
     path('admin/', admin.site.urls),
 
     # JWT-токены
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    # Основные DRF-роуты из assets/urls.py
+    # DRF-маршруты
     path('api/', include('assets.urls')),
 
-    # Веб-карточка (HTML) для DetailView
+    # веб-деталка (HTML-карточка) через DetailView
     path(
         'asset/<uuid:pk>/',
         DetailView.as_view(
@@ -44,6 +41,7 @@ urlpatterns = [
     ),
 ]
 
-# Отдаём статику и медиа всегда (не только в DEBUG)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# WhiteNoise уже отдаёт STATIC_URL, поэтому не добавляем static() для статики
+
+# Но для MEDIA_URL (фото) оставляем Django-статик
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- assets/serializers.py
+++ assets/serializers.py
@@ -1,29 +1,63 @@
+# assets/serializers.py
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from django.urls import reverse
 from .models import Asset
 
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+
+    def create(self, validated_data):
+        return User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    assets_count = serializers.IntegerField(source='assets.count', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'assets_count')
+
+
 class AssetSerializer(serializers.ModelSerializer):
-    photo_url = serializers.SerializerMethodField()
-    qr_url    = serializers.SerializerMethodField()
+    owner_username = serializers.CharField(source='owner.username', read_only=True)
+    photo_url      = serializers.SerializerMethodField()
+    qr_url         = serializers.SerializerMethodField()
 
     class Meta:
         model = Asset
         fields = [
-            'id','asset_type','title','description',
-            'photo_url','status',
-            'owner_username','created_at','qr_url',
+            'id', 'asset_type', 'title', 'description',
+            'photo_url', 'status', 'owner', 'owner_username', 'pending_user',
+            'created_at', 'qr_url'
         ]
+        read_only_fields = ['owner', 'created_at', 'qr_url']
 
     def get_photo_url(self, obj):
-        if not obj.photo:
-            return None
         request = self.context.get('request')
-        return request.build_absolute_uri(obj.photo.url)
+        if obj.photo and request:
+            return request.build_absolute_uri(obj.photo.url)
+        return None
 
     def get_qr_url(self, obj):
         if obj.status != 'assigned':
             return None
         request = self.context.get('request')
+        if not request:
+            return None
         return request.build_absolute_uri(
             reverse('asset-qr', args=[obj.id])
         )
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True)
